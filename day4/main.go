1aad9772ea7827e1115dea5c3c6e5ccf1dd26507
Andy <heandy11@yahoo.com>
2024-12-09 22:17:49 -0800

    misinterpreted... wrong ans - day4.1

--- day4/main.go
+++ day4/main.go
@@ -7,13 +7,43 @@ import (
 	"os"
 )
 
-func part1(graph [][]string) int {
+func dfs(graph [][]byte, target string, r int, c int) bool {
+	// handle base case (when we found the word)
+	if len(target) == 0 {
+		return true
+	}
+
+	// handle invalid cases:
+	// - out of bounds
+	// - word we are at doesn't match what we want
+	if r < 0 || r >= len(graph) || c < 0 || c >= len(graph[0]) || graph[r][c] != target[0] {
+		return false
+	}
+
+	tempVisited := graph[r][c] // to undo the decision
+	graph[r][c] = '.'          // make the decision to visit
+	found := dfs(graph, target[1:], r+1, c) || dfs(graph, target[1:], r-1, c) || dfs(graph, target[1:], r, c+1) || dfs(graph, target[1:], r, c-1) ||
+		dfs(graph, target[1:], r+1, c+1) || dfs(graph, target[1:], r-1, c-1) || dfs(graph, target[1:], r+1, c-1) || dfs(graph, target[1:], r-1, c+1)
+	graph[r][c] = tempVisited // undo the decision
+	return found
+}
+
+func part1(graph [][]byte) int {
 	count := 0
 	fmt.Printf("%v\n", graph)
 	// go line by line and character by character
 	// look for 'X' since that is your starting point
 	// visit each neighbor recursively and at each collection, if I am collecting a character I want, continue visiting
 	// if I collected a character that isn't what I want, break out of the visit path
+	for r, line := range graph {
+		for c := range line {
+			found := dfs(graph, "XMAS", r, c)
+			if found {
+				count += 1
+				fmt.Printf("DFS found at [%v][%v]\n", r, c)
+			}
+		}
+	}
 	return count
 }
 
@@ -28,16 +58,16 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	graph := [][]string{}
+	graph := [][]byte{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		line_parsed := []string{}
+		line_parsed := []byte{}
 		for i := range line {
 			ch := line[i]
-			line_parsed = append(line_parsed, string(ch))
+			line_parsed = append(line_parsed, ch)
 		}
-		graph = append(graph, line_parsed)
+		graph = append(graph, []byte(line_parsed))
 	}
-	part1(graph)
+	fmt.Printf("part1: %v\n", part1(graph))
 }
