f1021d7532c49e87c2dcefe185855f32bfa0f2c9
Andy <heandy11@yahoo.com>
2024-12-11 21:31:48 -0800

    day4 part1 done

--- day4/main.go
+++ day4/main.go
@@ -28,7 +28,7 @@ func dfs(graph [][]byte, target string, r int, c int) bool {
 	return found
 }
 
-func part1(graph [][]byte) int {
+func part1_d(graph [][]byte) int {
 	count := 0
 	fmt.Printf("%v\n", graph)
 	// go line by line and character by character
@@ -47,9 +47,45 @@ func part1(graph [][]byte) int {
 	return count
 }
 
+func searchInDirection(graph [][]byte, r int, c int, direction []int, goal []byte) bool {
+	// goal is to find 'M', 'A', 'S' when doing: r + direction[0], c + direction[1] three times
+	// if, when going in a direction we are out of bounds, return false
+	// if, when collecting a new character, if it isn't currently what we want next, return false
+	for _, char := range goal {
+		r += direction[0]
+		c += direction[1]
+		if r < 0 || r >= len(graph) || c < 0 || c >= len(graph[0]) || graph[r][c] != char {
+			return false
+		}
+	}
+	return true
+}
+
+func part1(graph [][]byte) int {
+	count := 0
+	fmt.Printf("%v\n", graph)
+	// go line by line, char by char
+	// if current char is an x, look straight in one of 8 directions (within bounds)
+	// if a search in one direction 3 letters deep matches "MAS", return true for that direction
+	directions := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}
+	goal := []byte{'M', 'A', 'S'}
+	for r, line := range graph {
+		for c := range line {
+			if graph[r][c] == 'X' {
+				for _, direction := range directions {
+					if searchInDirection(graph, r, c, direction, goal) {
+						count += 1
+					}
+				}
+			}
+		}
+	}
+	return count
+}
+
 func main() {
 	// convert file to 2d array of strs or chars?
-	file, err := os.Open("./test.txt")
+	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatalf("Error reading input file: %v\n", err)
 	}
